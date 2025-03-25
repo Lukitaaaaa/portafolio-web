13ea1d6ae7a7d1378756b865152309ceeb6f1e28
Lukitaaaaa <lucasomargallardo@gmail.com>
2025-03-25 19:25:31 -0300

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://Lukitaaaaa.github.io', // Your website URL
+  base: 'portafolio-web'
 });
